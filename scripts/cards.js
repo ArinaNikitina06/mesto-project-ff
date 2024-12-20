372a08e1559bb3a526e7c799d7b76b9a71d23cb4
Arina <arinashkapa@yandex.ru>
2024-12-20 14:38:55 +0100

    changers

--- scripts/cards.js
+++ scripts/cards.js
@@ -25,29 +25,4 @@ const initialCards = [
   },
 ];
 
-const placesList = document.querySelector(".places__list");
 
-
-function createCards(initialCards) {
-  initialCards.forEach((element) => createCard(element));
-}
-
-createCards(initialCards);
-
-function createCard({ name, link }) {
-  const getTemplateCard = document
-    .querySelector("#card-template")
-    .content.querySelector(".card");
-    
-  const card = getTemplateCard.cloneNode(true);
-  card.querySelector(".card__title").textContent = name
-  card.querySelector(".card__image").src = link
-card.querySelector(".card__delete-button").addEventListener('click', removeCard)
-  
-
-  placesList.insertAdjacentElement('beforeend', card)
-}
-
-function removeCard(event){
-  event.target.closest(".card").remove()
-}
