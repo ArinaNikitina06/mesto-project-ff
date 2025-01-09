12730f78eae85db82a9b415cbd8a447eb8a84aa5
Arina <arinashkapa@yandex.ru>
2025-01-09 08:47:32 +0100

    open and close popups:edit and create

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,17 +12,58 @@ import initialCards from "./cards.js";
 import "../pages/index.css";
 
 const placesList = document.querySelector(".places__list");
-const popupProfileOpenButton = document.querySelector(".profile__edit-button");
-const popupProfileEdit = document.querySelector(".popup");
-const popupProfileEditCloseBtn = popupProfileEdit.querySelector('.popup__close');
+const popupProfileOpenButton = document.querySelector(".profile__edit-button")
+const popupPlaceOpenButton = document.querySelector(".profile__add-button");
 
-popupProfileOpenButton.addEventListener('click', () => {
-popupProfileEdit.classList.add("popup_is-opened");
-})
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupNewPlace = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup_type_image");
 
-popupProfileEditCloseBtn.addEventListener("click", () => {
-  popupProfileEdit.classList.remove("popup_is-opened");
+const popupList = document.querySelectorAll(".popup");
+// const popupProfileEditCloseBtn = popupProfileEdit.querySelector('.popup__close');
+
+function openPopUp(popup) {
+  document.addEventListener('keyup', closePopupByEsc)
+  popup.classList.add("popup_is-opened");
+}
+
+function closePopUp(popup) {
+  document.removeEventListener('keyup', closePopupByEsc)
+  popup.classList.remove("popup_is-opened");
+}
+
+popupProfileOpenButton.addEventListener("click", () => {
+  openPopUp(popupEdit);
 });
+
+popupPlaceOpenButton.addEventListener("click", () => {
+  openPopUp(popupNewPlace);
+});
+
+
+popupList.forEach(popup => {
+  popup.addEventListener("click", (e) => {
+  if (e.target.closest(".popup__close")) {
+    closePopUp(popup);
+  }
+
+  if (e.target === e.currentTarget) {
+    closePopUp(popup);
+  }
+});
+}); 
+
+function closePopupByEsc(e){
+    if (e.key === 'Escape') {
+      const currentOpenPopup = document.querySelector('.popup_is-opened')
+          closePopUp(currentOpenPopup);
+    }
+}
+
+
+// popupProfileEditCloseBtn.addEventListener("click", () => {
+//   closePopUp(popup)
+// });
 
 function createCards(initialCards) {
   initialCards.forEach((element) => {
@@ -50,4 +91,3 @@ function createCard({ name, link }) {
 function removeCard(event) {
   event.target.closest(".card").remove();
 }
-
