be1704ae90cbc5abd88df121ee885bc0005bd459
Arina <arinashkapa@yandex.ru>
2025-01-29 11:08:58 +0100

    update validation logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -75,9 +75,11 @@ const configPopupEditForm = {
         min: 2,
         max: 40,
       },
+      isUrl: false,
       errorMessages: {
         isRequired: "поле обязательно для заполнения",
         between: "В поле «Имя» должно быть от 2 до 40 символов",
+        isUrl: "«Ссылка на картинку» должен быть URL",
       },
     },
     description: {
@@ -86,9 +88,49 @@ const configPopupEditForm = {
         min: 2,
         max: 400,
       },
+      isUrl: false,
       errorMessages: {
         isRequired: "поле обязательно для заполнения",
         between: "В поле «Описание» должно быть от 2 до 400 символов",
+        isUrl: "Ссылка на картинку» должен быть URL",
+      },
+    },
+  },
+  buttonRule: {
+    disabled: true,
+  },
+};
+const configPopupCreateNewPlaceForm = {
+  selectors: {
+    input: ".popup__input",
+    spanError: ".popup__input-error",
+    button: ".popup__button",
+  },
+  inputsRule: {
+    ["place-name"]: {
+      isRequired: true,
+      between: {
+        min: 2,
+        max: 30,
+      },
+      isUrl: false,
+      errorMessages: {
+        isRequired: "поле обязательно для заполнения",
+        between: "В поле «Название» должно быть от 2 до 30 символов",
+        isUrl: "«Ссылка на картинку» должен быть URL",
+      },
+    },
+    link: {
+      isRequired: true,
+      between: {
+        min: 2,
+        max: 10000,
+      },
+      isUrl: true,
+      errorMessages: {
+        isRequired: "поле обязательно для заполнения",
+        between: "В поле 'Ссылка на картинку' должно быть от 2 до 10000 символов",
+        isUrl: "'Ссылка на картинку' должен быть URL",
       },
     },
   },
@@ -97,7 +139,7 @@ const configPopupEditForm = {
   },
 };
 
-function validateForm(form, config) {
+function enableValidation(form, config) {
   const inputsList = form.querySelectorAll(config.selectors.input);
   const currentBtn = form.querySelector(config.selectors.button);
   const spansList = form.querySelectorAll(config.selectors.spanError);
@@ -108,9 +150,19 @@ function validateForm(form, config) {
     );
 
     currentInput.addEventListener("input", (e) => {
-      const rulesCurrentInput = config.inputsRule[currentInput.name
-        
-      ];
+      const rulesCurrentInput = config.inputsRule[currentInput.name];
+
+      // error isRequired
+      if (
+        rulesCurrentInput.isRequired === true &&
+        currentInput.value.trim().length === 0
+      ) {
+        currentInput.classList.add("popup__input_type-error");
+        currentSpan.classList.add("popup__input-error_active");
+        currentSpan.textContent = rulesCurrentInput.errorMessages.isRequired;
+        currentBtn.setAttribute("disabled", "disabled");
+        return;
+      }
 
       // error between
       if (
@@ -121,44 +173,58 @@ function validateForm(form, config) {
         currentSpan.classList.add("popup__input-error_active");
         currentSpan.textContent = rulesCurrentInput.errorMessages.between;
         currentBtn.setAttribute("disabled", "disabled");
+        return;
       }
-      
-      // error isRequired
-      if (rulesCurrentInput.isRequired === true && currentInput.value.trim().length === 0) {
+
+      if (
+        (rulesCurrentInput.isUrl === true &&
+          !currentInput.value.includes("http://")) &&
+        !currentInput.value.includes("https://")
+      ) {
         currentInput.classList.add("popup__input_type-error");
         currentSpan.classList.add("popup__input-error_active");
-        currentSpan.textContent = rulesCurrentInput.errorMessages.isRequired
+        currentSpan.textContent = rulesCurrentInput.errorMessages.isUrl;
         currentBtn.setAttribute("disabled", "disabled");
+        return;
       }
 
       // success, clean error
-      if (
-         currentInput.value.trim().length > rulesCurrentInput.between.min &&
-         currentInput.value.trim().length < rulesCurrentInput.between.max
-       ) {
-         currentInput.classList.remove("popup__input_type-error");
-         currentSpan.classList.remove("popup__input-error_active");
-         currentSpan.textContent = '';
-         currentBtn.removeAttribute("disabled");
-       }
+        currentInput.classList.remove("popup__input_type-error");
+        currentSpan.classList.remove("popup__input-error_active");
+        currentSpan.textContent = "";
+        currentBtn.removeAttribute("disabled");
     });
   });
 
-  function cleanAllErrors() {
-    inputsList.forEach((currentInput) => {
+  // function cleanAllErrors() {
+  //   inputsList.forEach((currentInput) => {
+  //     currentInput.classList.remove("popup__input_type-error");
+  //   });
+
+  //   spansList.forEach((currentSpan) => {
+  //     currentSpan.classList.remove("popup__input-error_active");
+  //   });
+  //   currentBtn.removeAttribute("disabled");
+  // }
+
+  // return cleanAllErrors;
+}
+
+ function clearValidation(form, config) {
+  const inputsList = form.querySelectorAll(config.selectors.input);
+  const currentBtn = form.querySelector(config.selectors.button);
+  const spansList = form.querySelectorAll(config.selectors.spanError);
+
+  inputsList.forEach((currentInput) => {
       currentInput.classList.remove("popup__input_type-error");
     });
 
- spansList.forEach((currentSpan) => {
-   currentSpan.classList.remove("popup__input-error_active");
-});
-   currentBtn.removeAttribute("disabled");
-  } 
+    spansList.forEach((currentSpan) => {
+      currentSpan.classList.remove("popup__input-error_active");
+    });
+    currentBtn.removeAttribute("disabled");
+ }
 
-  return cleanAllErrors
-}
-
-const cleanAllErrors = validateForm(popupEditForm, configPopupEditForm);
 
 
 // popupEditProfileTitleInput.addEventListener('input', (e) => {
@@ -192,14 +258,21 @@ const cleanAllErrors = validateForm(popupEditForm, configPopupEditForm);
 
 // })
 
+enableValidation(popupEditForm, configPopupEditForm);
+enableValidation(
+  popupNewPlaceForm,
+  configPopupCreateNewPlaceForm
+);
+
 popupProfileOpenButton.addEventListener("click", () => {
   popupEditProfileTitleInput.value = profileTitle.textContent;
   popupEditProfileDescriptionInput.value = profileDescription.textContent;
-  cleanAllErrors()
+  clearValidation(popupEditForm, configPopupEditForm);
   openPopUp(popupEdit);
 });
 
 popupPlaceOpenButton.addEventListener("click", () => {
+  clearValidation(popupNewPlaceForm, configPopupCreateNewPlaceForm);
   openPopUp(popupNewPlace);
 });
 
