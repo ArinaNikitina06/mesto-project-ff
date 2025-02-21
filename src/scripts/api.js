6c5ae997a776bb5a5704226cb927ae5be226abbf
Arina <arinashkapa@yandex.ru>
2025-02-21 08:40:52 +0100

    update api js

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,63 +1,77 @@
+function handleResponce(res){
+    if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+}
+
 function addLike({ baseUrl, headers }) {
   return fetch(baseUrl, {
     method: "PUT",
-		headers,
+    headers,
   })
-    .then((res) => res.json())
-    .then((result) => result);
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 function delLike({ baseUrl, headers }) {
   return fetch(baseUrl, {
     method: "DELETE",
-		headers,
+    headers,
   })
-    .then((res) => res.json())
-    .then((result) => result);
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 function delCard({ baseUrl, headers }) {
   return fetch(baseUrl, {
     method: "DELETE",
-		headers,
+    headers,
   })
-    .then((res) => res.json())
-    .then((result) => result);
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 function getUserData({ baseUrl, headers }) {
   return fetch(baseUrl, {
-		headers
-	})
-    .then((res) => res.json())
-    .then((result) => result);
+    headers,
+  })
+    .then(handleResponce )
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 function getCards({ baseUrl, headers }) {
   return fetch(baseUrl, {
-		headers
-	})
-    .then((res) => res.json())
-    .then((result) => result);
+    headers,
+  })
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 function updateUserPlace({ baseUrl, headers }, payload) {
   return fetch(baseUrl, {
     method: "POST",
     body: JSON.stringify(payload),
-		headers,
+    headers,
   })
-    .then((res) => res.json())
-    .then((result) => result);
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 function updateUserData({ baseUrl, headers }, payload) {
   return fetch(baseUrl, {
     method: "PATCH",
     body: JSON.stringify(payload),
-		headers,
+    headers,
   })
-    .then((res) => res.json())
-    .then((result) => result);
+    .then(handleResponce)
+    .then((result) => result)
+    .catch((error) => console.log("my error ->", error));
 }
 
 export {
